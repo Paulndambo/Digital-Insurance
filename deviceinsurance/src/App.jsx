efd0f733fcec0fe9d0cca754ea1ca1c9a4198a5c
Paul Ndambo <paulkadabo@gmail.com>
2026-01-22 17:35:46 +0300

    Desktop mode set as default

--- deviceinsurance/src/App.jsx
+++ deviceinsurance/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DeviceInsurance from './DeviceInsurance'
+import MobileWarning from './components/MobileWarning'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <MobileWarning />
       <DeviceInsurance />
     </>
   )
